31d3de653f905f1e1d2b9f09aa082a2acb9bafc4
Stephen Yoon <xbowmaster2020@gmail.com>
2023-02-07 11:07:48 -0500

    trying to get saveSong() to work pt 1

--- equalizer2.js
+++ equalizer2.js
@@ -9,7 +9,7 @@
 let play_button = document.querySelector('#play_button');
 let reverb_button = document.querySelector('#reverb_button');
 let save_button = document.querySelector("#save_button");
-let button_8d = document.querySelector("8d_button");
+let button_8d = document.querySelector("#button_8d");
 let song, reverb, speed_slider, pan_slider;
 let reverb_on = false;
 let on_8d = false;
@@ -22,39 +22,23 @@ function setup() {
   speed_slider = createSlider(0.6, 1.5, 1, 0.1); // min, max, start, step
   pan_slider = createSlider(-1, 1, 0, 0.1)
   reverb = new p5.Reverb();
-  
-  reverb.process(reverbFile, grabDuration(), grabRate());
-  reverb.amp(); 
-  // Fix it not working, no clue why it doesn't work -- Maybe greyed out variables clue 
-}
-
-function grabDuration() {
-  //Grabbing reverb duration from 
-  const btn= document.getElementById("btn");
-  btn.addEventListener('click', function(){
-  var dur = document.getElementById("reverbdur").value;
-  });
-  if (dur > song.duration()){
-    dur = song.duration();
-  }
-  return dur;
-}
-function grabRate(){
-  const btn= document.getElementById("btn");
-  btn.addEventListener('click', function(){
-  var rate = document.getElementById("reverbRate").value;
-  });
-  if (rate > 100){
-    rate = 100;
-  }
-  return rate;
+  reverb.process(song, 10, 10); // 10 seconds duration, 10% decay
 }
 
 function draw() {
   let val = 0;
   if (reverb_on) val = 1; 
+  if (on_8d) {
+    song.pan(-1);
+  } else {
+    song.pan(pan_slider.value())
+  }
+
   reverb.drywet(val); // 1 is all reverb
   song.rate(speed_slider.value());
+  song.onended(() => {
+    play_button.innerText = "Play";
+  });
 }
 
 function togglePlay() {
@@ -72,6 +56,7 @@ function toggle8d() {
     on_8d = true;
     button_8d.innerText = 'off';
   } else {
+    on_8d = false;
     button_8d.innerText = 'on';
   }
 }
