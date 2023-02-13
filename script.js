a4b310d459d1b33b2d5b57f86020a917f3fe46dd
ehnry055 <henry.g.choi@gmail.com>
2023-02-12 19:44:14 -0500

    saving changes lol

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 var button = document.getElementById('actualbtn');
 var fileChosen = document.getElementById("file-chosen");
-var selectedFile;
 var gallery = document.getElementById("gallery");
-
+let selectedFile;
 
 button.addEventListener("change", function(){
   fileChosen.textContent = this.files[0].name;
-  selectedFile = document.getElementById("input").files;
+  selectedFile = document.getElementById("input").files[0];
+  let db;
+  const request = indexedDB.open("MyTestDatabase");
+  request.onerror = (event) => {
+    console.error("it broke :(");
+  };
+  request.onsuccess = (event) => {
+    db = event.target.result;
+  };
+});
 
+actualtbtn.onchange = e =>{
   const reader = new FileReader();
   reader.onload = function(){
     var str = this.result;
-    localStorage.setItem("song", "str");
-  };
-});
+    localStorage.setItem("song", str);
+    const aud = new Audio(str);
+    aud.play();
+    };
+  reader.readAsDataURL(f.files[0]);
+  }
